e34543921bda24c4724952e4eea0391445755a0a
keigomichi <kato_keigo@cyberagent.co.jp>
2025-04-28 10:21:13 +0900

    test(mobile-app): update example

--- mobile-app/integration_test/example_test.dart
+++ mobile-app/integration_test/example_test.dart
@@ -1,12 +1,27 @@
-import 'package:architecture_templates/main.dart';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:patrol/patrol.dart';
 
 void main() {
-  patrolTest('real app test', ($) async {
-    // Do all the necessary setup here (DI, services, etc.)
+  patrolTest(
+    'counter state is the same after going to home and switching apps',
+    ($) async {
+      // Replace later with your app's main widget
+      await $.pumpWidgetAndSettle(
+        MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(title: const Text('app')),
+            backgroundColor: Colors.blue,
+          ),
+        ),
+      );
 
-    await $.pumpWidgetAndSettle(const MainApp()); // Your's app main widget
-
-    // Start testing your app here
-  });
+      expect($('app'), findsOneWidget);
+      if (!Platform.isMacOS) {
+        await $.native.pressHome();
+      }
+    },
+  );
 }
